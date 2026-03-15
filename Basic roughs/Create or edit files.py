8428554ff9478cd364f4255ba6cbd09f60dc1879
grey <greyscale021@gmail.com>
2026-03-16 00:08:53 +0600

    Re-structured the repo

--- Basic roughs/Create or edit files.py
+++
@@ -1,4 +0,0 @@
-employee_file = open("index.html", "r+")
-employee_file.write("<html><body><h1>Hello World!</h1>")
-print(employee_file.read())
-employee_file.close()
--- Basic roughs/Example of functions/Pytest.py
+++
@@ -1,9 +0,0 @@
-#installed pytest by "pip install pytest"
-#so now all of the functions with "test_" will run as test cases. 
-def is_even(n):
-    return n%2 == 0
-def test_is_even():
-    assert is_even(2) == True
-def test_is_odd():
-
-    assert is_even(3) == False
